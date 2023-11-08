db637660df6608c5aea9eeb452ed8f52709ffc00
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-11-08 15:08:46 +1100

    trying to get prefabs to switch after 5 secs

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class Ability : MonoBehaviour
 {
     public GameObject box;
-    public GameObject interactivebox;
+    public GameObject box2;
+    public GameObject interactiveboxPrefab;
+    
+    public GameObject aura;
+
+    float currentTime = 0f;
+    float startingTime = 0f;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        currentTime -= Time.deltaTime;
+
+        if (currentTime < 0)
+        {
+            box.SetActive(true);
+            aura.SetActive(false);
+            box2.SetActive(true);
+            Destroy(interactiveboxPrefab);
+        }
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             box.SetActive(false);
-            interactivebox.SetActive(true);
+            box2.SetActive(false);
+            Instantiate(interactiveboxPrefab, box.transform.position, Quaternion.identity);
+            aura.SetActive(true);
+
+            currentTime = 5f;
         }
     }
 }
