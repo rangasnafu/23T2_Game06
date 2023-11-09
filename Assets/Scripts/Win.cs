484aebac6e392a017aa7e522e80a003370f958d8
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-11-10 01:30:23 +1100

    gamefinished

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public void WinGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
