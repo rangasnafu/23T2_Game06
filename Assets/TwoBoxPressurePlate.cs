e3554ec922b47af4f75cae1b172272bd93802a85
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-11-10 00:42:00 +1100

    two box pressure plate

---
+++ Assets/TwoBoxPressurePlate.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoBoxPressurePlate : MonoBehaviour
+{
+    public GameObject doorToDeactivate;
+    public GameObject explosionEffect;
+
+    private bool box1Inside;
+    private bool box2Inside;
+
+    private bool doorDeactivated = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Box1"))
+        {
+            box1Inside = true;
+            TryOpenDoor();
+        }
+        else if (other.CompareTag("Box2"))
+        {
+            box2Inside = true;
+            TryOpenDoor();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Box1"))
+        {
+            box1Inside = false;
+        }
+        else if (other.CompareTag("Box2"))
+        {
+            box2Inside = false;
+        }
+    }
+
+    private void TryOpenDoor()
+    {
+        if (box1Inside && box2Inside && !doorDeactivated)
+        {
+            //Destroy(doorToDeactivate);
+            //Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+            Vector3 doorPosition = doorToDeactivate.transform.position;
+
+            StartCoroutine(DeactivateAndExplode());
+        }
+    }
+
+    private IEnumerator DeactivateAndExplode()
+    {
+        yield return new WaitForSeconds(1.0f); // Wait for 1 second
+
+        Destroy(doorToDeactivate);
+        doorDeactivated = true;
+
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, doorToDeactivate.transform.position, Quaternion.identity);
+        }
+    }
+}
